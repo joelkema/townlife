d57baededfd0cc6a37adcbd2bb06e87935f20399
= <=>
2023-10-11 22:09:19 +0200

    add some documentation to important function

--- src/utils/gameTime.ts
+++ src/utils/gameTime.ts
@@ -14,22 +14,38 @@ const settings = {
 
 // real time
 export const ticksPerInGameDay = settings.dwarfFortress.ticksPerInGameDay; 
-export const ticksPerInGameHour = ticksPerInGameDay / 24; //2500
+export const ticksPerInGameHour = ticksPerInGameDay / 24; //2500 if 60000 ticks per day
 
-export const getInGameTime = (tick: number) => {
+/**
+ * Converts a number of ticks to in-game time.
+ * @param {number} ticks - The number of ticks to convert.
+ * @returns {{minutes: number, hours: number, days: number}} - An object containing the in-game time.
+ */
+export const getInGameTime = (ticks: number) => {
     const ticksPerInGameMinute = ticksPerInGameHour / 60;
-    const totalMinutes = Math.floor(tick / ticksPerInGameMinute);
-    const totalHours = Math.floor(tick / ticksPerInGameHour);
+    const totalMinutes = Math.floor(ticks / ticksPerInGameMinute);
+    const totalHours = Math.floor(ticks / ticksPerInGameHour);
 
     const minutes = totalMinutes % 60;
-    const hours = tickToHours(tick);
+    const hours = tickToHours(ticks);
     const days = Math.floor(totalHours / 24);
 
     return { minutes, hours, days };
 };
 
+/**
+ * Converts a number of ticks to in-game days.
+ * @param {number} ticks - The number of ticks to convert.
+ * @returns {number} - The number of in-game days.
+ */
 export const tickToHours = (tick: number) => Math.floor(tick / ticksPerInGameHour) % 24;
 
 const formatNr = (nr: number) => (nr < 10 ? `0${nr}` : nr);
+
+/**
+ * Formats a time object to a string.
+ * @param {{minutes: number, hours: number, days: number}} time - The time object to format.
+ * @returns {string} - The formatted time.
+ */
 export const formatTime = (hours: number, minutes: number) =>
     `${formatNr(hours)}:${formatNr(minutes)}`;
